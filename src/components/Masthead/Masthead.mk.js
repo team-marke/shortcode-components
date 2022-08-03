d61bb46ca2f5d335a5149bd9315d6c6fafb888ff
marke-franco-nienow <franco@marke.com.br>
2022-08-03 16:35:32 -0300

    Componente Button

--- src/components/Masthead/Masthead.mk.js
+++
@@ -1,9 +0,0 @@
-const CloudinaryImageURL = require("../../core-components/CloudinaryImageURL");
-
-function Masthead(content, img) {
-  return `
-    <div class="masthead">${CloudinaryImageURL(img)}</div>
-  `;
-}
-
-module.exports = Masthead;
